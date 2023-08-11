6081efdb20521072c1fa4d28bf2ee121dcaad8c0
Dan Munckton <munckfish@gmail.com>
2023-08-11 09:05:04 +0100

    Define Each, page and entry

--- bplustree/bplustree.go
+++ bplustree/bplustree.go
@@ -1,19 +1,51 @@
 package bplustree
 
-type Tree[K any, V any] struct {
+import (
+	"golang.org/x/exp/constraints"
+	"golang.org/x/exp/slices"
+)
+
+type entry[K any, V any] struct {
 	key   K
 	value V
 }
 
-func New[K any, V any]() Tree[K, V] {
+type page[K constraints.Ordered, V any] struct {
+	entries []entry[K, V]
+}
+
+func (p *page[K, V]) add(key K, value V) {
+	newEntry := entry[K, V]{key: key, value: value}
+	p.entries = append(p.entries, newEntry)
+	slices.SortFunc(p.entries, func(e1, e2 entry[K, V]) bool { return e1.key < e2.key })
+}
+
+type Tree[K constraints.Ordered, V any] struct {
+	root *page[K, V]
+}
+
+func New[K constraints.Ordered, V any]() Tree[K, V] {
 	return Tree[K, V]{}
 }
 
 func (t *Tree[K, V]) Insert(key K, value V) {
-	t.key = key
-	t.value = value
+	if t.root == nil {
+		t.root = &page[K, V]{}
+	}
+	t.root.add(key, value)
 }
 
-func (t *Tree[K, V]) Find(key K) V {
-	return t.value
+func (t *Tree[K, V]) Find(key K) *V {
+	return &t.root.entries[0].value
+}
+
+func (t *Tree[K, V]) Each(f func(K, V)) {
+	t.traverseSubtree(t.root, f)
+	// f(t.root.entries[0].key, t.root.entries[0].value)
+}
+
+func (t *Tree[K, V]) traverseSubtree(page *page[K, V], f func(K, V)) {
+	for _, entry := range page.entries {
+		f(entry.key, entry.value)
+	}
 }
--- bplustree/bplustree_test.go
+++ bplustree/bplustree_test.go
@@ -7,16 +7,42 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBPlusTree(t *testing.T) {
-	// Given
-	bt := bplustree.New[int, string]()
-	bt2 := bplustree.New[string, int]()
+func TestBPlusTreeInsert(t *testing.T) {
+	t.Run("it supports generic key/value pairs", func(t *testing.T) {
+		// Given
+		bt1 := bplustree.New[int, string]()
+		bt2 := bplustree.New[string, int]()
 
-	// When
-	bt.Insert(123, "some value")
-	bt2.Insert("some key", 123)
+		// When
+		bt1.Insert(123, "some value")
+		bt2.Insert("some key", 123)
 
-	// Then
-	require.Equal(t, bt.Find(123), "some value")
-	require.Equal(t, bt2.Find("some key"), 123)
+		// Then
+		require.Equal(t, bt1.Find(123), ptr("some value"))
+		require.Equal(t, bt2.Find("some key"), ptr(123))
+	})
+}
+
+func TestBPlusTreeEach(t *testing.T) {
+	t.Run("it performs in-order traversal of entries based on keys", func(t *testing.T) {
+		// Given
+		type emptyValue = struct{}
+		bt := bplustree.New[int, emptyValue]()
+		bt.Insert(2, emptyValue{})
+		bt.Insert(1, emptyValue{})
+		bt.Insert(3, emptyValue{})
+
+		// When
+		var collected []int
+		bt.Each(func(key int, _ emptyValue) {
+			collected = append(collected, key)
+		})
+
+		// Then
+		require.Equal(t, []int{1, 2, 3}, collected)
+	})
+}
+
+func ptr[T any](v T) *T {
+	return &v
 }
