6ff07a6bde57dffaa5445dade148a9395eabd0fe
Dan Munckton <munckfish@gmail.com>
2023-08-15 08:26:27 +0100

    Extract invariants to a separate file

--- bplustree/bplustree.go
+++ bplustree/bplustree.go
@@ -1,8 +1,6 @@
 package bplustree
 
 import (
-	"fmt"
-
 	"golang.org/x/exp/constraints"
 	"golang.org/x/exp/slices"
 )
@@ -86,21 +84,6 @@ func (t *Tree[K, V]) Each(f func(K, V)) {
 	}
 }
 
-func (t *Tree[K, V]) AssertInvariantsHold() {
-	t.root.validateSubtree()
-}
-
-func (t *Tree[K, V]) CheckInvariantsHold() (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("%v", r)
-		}
-	}()
-
-	t.AssertInvariantsHold()
-	return nil
-}
-
 type entry[K any, V any] struct {
 	key   K
 	value V
@@ -120,6 +103,19 @@ func (p *page[K, V]) add(key K, value V) {
 	newEntry := entry[K, V]{key: key, value: value}
 	p.entries = append(p.entries, newEntry)
 	p.sort()
+}
+
+func (p *page[K, V]) firstEntry() *entry[K, V] {
+	if len(p.entries) == 0 {
+		return nil
+	}
+	return &p.entries[0]
+}
+func (p *page[K, V]) lastEntry() *entry[K, V] {
+	if len(p.entries) == 0 {
+		return nil
+	}
+	return &p.entries[len(p.entries)-1]
 }
 
 func (p *page[K, V]) sort() {
@@ -172,76 +168,4 @@ func (p *page[K, V]) doFind(key K, parent *page[K, V]) (*entry[K, V], *page[K, V
 	}
 
 	return nil, p
-}
-
-func (p *page[K, V]) validateSubtree() {
-	if p == nil {
-		return
-	}
-
-	for _, child := range p.children {
-		child.validateSubtree()
-	}
-
-	p.validate()
-}
-
-func (p *page[K, V]) firstEntry() *entry[K, V] {
-	if len(p.entries) == 0 {
-		return nil
-	}
-	return &p.entries[0]
-}
-func (p *page[K, V]) lastEntry() *entry[K, V] {
-	if len(p.entries) == 0 {
-		return nil
-	}
-	return &p.entries[len(p.entries)-1]
-}
-
-func (p *page[K, V]) validate() {
-	isLeaf := len(p.children) == 0
-	isRoot := p.parent == nil
-
-	// 1. Every page has at most 2k+1 children.
-	assert("too many children", len(p.children) <= maxChildren)
-
-	// 1.1 Every page has at most 2k entries.
-	assert("too many entries", len(p.entries) <= maxEntries)
-
-	// 2. Every non-leaf page (except root) has at least ⌈(2k+1)/2⌉ child pages.
-	if !isLeaf && !isRoot {
-		assert("too few children", minChildren <= len(p.children))
-	}
-
-	// 3. The root has at least two children if it is not a leaf page.
-	if isRoot && !isLeaf {
-		assert("root has too few children", len(p.children) >= 2)
-	}
-
-	// 4. A non-leaf page with n children contains n−1 entries.
-	if !isLeaf {
-		assert("n children but not n-1 entries", len(p.entries) == len(p.children)-1)
-	}
-
-	if !isRoot {
-		assert("child has wrong parent", slices.Contains(p.parent.children[:], p))
-	}
-
-	if isLeaf {
-		return
-	}
-
-	for i, entry := range p.entries {
-		assert(fmt.Sprintf("NOT child[%d].lastEntry.key < entries[%d].key", i, i), p.children[i].lastEntry().key < entry.key)
-		// if len(p.children) > i+1 {
-		assert(fmt.Sprintf("NOT entries[%d].key < child[%d].firstEntry.key", i, i+1), entry.key < p.children[i+1].firstEntry().key)
-		// }
-	}
-}
-
-func assert(msg string, value bool) {
-	if !value {
-		panic(msg)
-	}
 }
---
+++ bplustree/invariant_test.go
@@ -0,0 +1,81 @@
+package bplustree
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/slices"
+)
+
+func (t *Tree[K, V]) AssertInvariantsHold() {
+	t.root.validateSubtree()
+}
+
+func (t *Tree[K, V]) CheckInvariantsHold() (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+
+	t.AssertInvariantsHold()
+	return nil
+}
+
+func (p *page[K, V]) validateSubtree() {
+	if p == nil {
+		return
+	}
+
+	for _, child := range p.children {
+		child.validateSubtree()
+	}
+
+	p.validate()
+}
+
+func (p *page[K, V]) validate() {
+	isLeaf := len(p.children) == 0
+	isRoot := p.parent == nil
+
+	// 1. Every page has at most 2k+1 children.
+	assert("too many children", len(p.children) <= maxChildren)
+
+	// 1.1 Every page has at most 2k entries.
+	assert("too many entries", len(p.entries) <= maxEntries)
+
+	// 2. Every non-leaf page (except root) has at least ⌈(2k+1)/2⌉ child pages.
+	if !isLeaf && !isRoot {
+		assert("too few children", minChildren <= len(p.children))
+	}
+
+	// 3. The root has at least two children if it is not a leaf page.
+	if isRoot && !isLeaf {
+		assert("root has too few children", len(p.children) >= 2)
+	}
+
+	// 4. A non-leaf page with n children contains n−1 entries.
+	if !isLeaf {
+		assert("n children but not n-1 entries", len(p.entries) == len(p.children)-1)
+	}
+
+	if !isRoot {
+		assert("child has wrong parent", slices.Contains(p.parent.children[:], p))
+	}
+
+	if isLeaf {
+		return
+	}
+
+	for i, entry := range p.entries {
+		assert(fmt.Sprintf("NOT child[%d].lastEntry.key < entries[%d].key", i, i), p.children[i].lastEntry().key < entry.key)
+		// if len(p.children) > i+1 {
+		assert(fmt.Sprintf("NOT entries[%d].key < child[%d].firstEntry.key", i, i+1), entry.key < p.children[i+1].firstEntry().key)
+		// }
+	}
+}
+
+func assert(msg string, value bool) {
+	if !value {
+		panic(msg)
+	}
+}
